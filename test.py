daaa8d57eca0270b5dccc6fd14e7e51f1c6b615d
Curtis Chong <curtischong5@gmail.com>
2024-12-14 12:08:17 -0800

    test can read dataset files

--- test.py
+++ test.py
@@ -1,44 +1,45 @@
+import numpy as np
+import h5py
+from dataclasses import dataclass
+import os
 
-import pathlib
-from diffusion.lattice_dataset import load_data
-from diffusion.lattice_helpers import matrix_to_params
-import torch
-
-def parse_dataset():
-    # density = mass / volume
-    # density = num atoms / volume of lattice
-    # to find the constnat c for the mean of the variance preservation of the lattice diffusion, we need to find the average density of the dataset
-
-    DATA_DIR = f"{pathlib.Path(__file__).parent.resolve()}/../datasets/alexandria_hdf5"
-
-
-    def main():
-        datasets = [
-            "alexandria_ps_000",
-            "alexandria_ps_001",
-            "alexandria_ps_002",
-            "alexandria_ps_003",
-            "alexandria_ps_004",
-            # "alexandria_ps_000_take10"
-        ]
-
-        unique_angles = set()
-
-        for dataset in datasets:
-            atomic_number_vector, lattice_matrix, _frac_x = load_data(
-                f"{DATA_DIR}/{dataset}.h5"
-            )
-            for i in range(len(atomic_number_vector)):
-                lengths, angles = matrix_to_params(
-                    torch.tensor(lattice_matrix[i]).unsqueeze(0)
-                )
-                for angle in angles[0]:
-                    unique_angles.add(angle.item())
-
-        print(list(unique_angles))
-        # Average density: 0.05539856385043283
-        # Average volume: 152.51649752530176
+@dataclass
+class Configuration:
+    # fields with a prefix "prev_" are the real target value. These are optional since during inference, we don't know them
+    atomic_numbers: np.ndarray  # this is a list of atomic numbers. NOT one-hot-encoded
+    X0: np.ndarray
+    L0: np.ndarray
 
 
-    if __name__ == "__main__":
-        main()
+def load_data(filepath: str):
+    # assert the file exists
+    assert os.path.isfile(filepath)
+
+    with h5py.File(filepath, "r") as file:
+        frac_x=file["crystals"]["frac_x"][:],
+        atomic_numbers=file["crystals"]["atomic_numbers"][:],
+        lattice=file["crystals"]["lattice"][:],
+        num_atoms=file["crystals"]["num_atoms"][:],
+        idx_start=file["crystals"]["idx_start"][:],
+
+    return frac_x[0], atomic_numbers[0], lattice[0], num_atoms[0], idx_start[0]
+
+
+def load_dataset(file_path) -> list[Configuration]:
+    frac_x, atomic_numbers, lattice, num_atoms, idx_start = load_data(file_path)
+
+    dataset = []
+    for i in range(lattice.shape[0]):
+        start = idx_start[i]
+        end = start + num_atoms[i]
+        config = Configuration(
+            atomic_numbers=atomic_numbers[start:end],
+            X0=frac_x[start:end],
+            L0=lattice[i],
+        )
+        dataset.append(config)
+    return dataset
+
+if __name__ == "__main__":
+    dataset = load_dataset("datasets/alexandria_hdf5/train_10.h5")
+    print(dataset)
