ef4ac0f6bcea6dd1e770cd48ffd2c1ae57a1e233
Curtis Chong <curtischong5@gmail.com>
2024-12-15 15:37:28 -0800

    wip fast algorithm

--- fast_approach.py
+++ fast_approach.py
@@ -5,7 +5,9 @@
 # a future optimization would be to use sram instead
 
 import numpy as np
-from create_graph import _compute_img_positions_torch, positions_to_graph
+from create_graph import _compute_img_positions_torch, points_in_parallelepiped, positions_to_graph
+import torch
+from pynanoflann import KDTree as NanoKDTree
 
 # we transform all the points to a normal 1x1x1 cube
 # then we just find the points near the edges of that cube
@@ -29,25 +31,65 @@ def normal_vectors(lattice):
 
     return n_a, -n_a, n_b, -n_b, n_c, -n_c
 
+def extend_lattice(lattice, radius):
+    lengths = torch.linalg.norm(lattice, axis=1)
+    unit_vectors = lattice / lengths[:, None]
+    extended_lattice = lattice + radius * unit_vectors
+    extended_lattice = extended_lattice - (radius * unit_vectors//2)
+    return extended_lattice
 
-def fast(*, lattice, frac_coord, radius: int = 5, max_number_neighbors: int, knn_library: str, n_workers: int = 1):
-    cart_coords = np.dot(frac_coord, lattice)
+def fast(*, lattice: torch.Tensor, frac_coord: torch.Tensor, radius: int = 5, max_number_neighbors: int, knn_library: str, n_workers: int = 1):
+    cart_coords = frac_coord @ lattice
 
-    for normal in normal_vectors(lattice):
-        # radius_for_side
-        supercell_positions = _compute_img_positions_torch(
-            positions=cart_coords, periodic_boundaries=lattice
-        )
-        normal_vector = ""
-        # how does the normal vector change AFTER we inverse the lattice?
-        # we can take a normal vector and run the reverse transformation
-        # the radius for the side is the NORM of the reversed vector
+    # radius_for_side
+    supercell_positions = _compute_img_positions_torch(
+        positions=cart_coords, periodic_boundaries=lattice
+    )
 
-    # we have all the coords. we just need to get the coords within the radius
-    # is there a fast bitwise way to just get the coords within the radius?
-    # it went to the norm of 
+    lattice2 = extend_lattice(lattice, radius)
 
-    return positions_to_graph(supercell_positions, positions=cart_coords, radius=radius, max_number_neighbors=max_number_neighbors, library=knn_library, n_workers=n_workers)
+    # the simplest way: just make a larger parallelepiped, then cross product each point with the larger parallelpied, and keep the points with a smaller cross product
+    lattice2_mask = points_in_parallelepiped(lattice2, supercell_positions)
+    supercell2_positions = torch.masked_select(supercell_positions, lattice2_mask.unsqueeze(-1))
+
+
+    # return positions_to_graph(supercell2_positions, positions=cart_coords, radius=radius, max_number_neighbors=max_number_neighbors, library=knn_library, n_workers=n_workers)
+
+def masked_positions_to_graph(supercell_positions, positions, radius, max_number_neighbors, knn_library, n_workers):
+    tree_data = supercell_positions.clone().detach().cpu().numpy()
+    tree_query = positions.clone().detach().cpu().numpy()
+    distance_upper_bound = np.array(radius) + 1e-8
+    num_positions = positions.shape[0]
+
+    tree = NanoKDTree(
+        n_neighbors=min(max_number_neighbors + 1, len(supercell_positions)),
+        radius=radius,
+        leaf_size=100,
+        metric="l2",
+    )
+    tree.fit(tree_data)
+    distance_values, nearest_img_neighbors = tree.kneighbors(
+        tree_query, n_jobs=n_workers
+    )
+    nearest_img_neighbors = nearest_img_neighbors.astype(np.int32)  # type: ignore
+
+    # remove the self node which will be closest
+    index_array = nearest_img_neighbors[:, 1:]
+    # remove distances greater than radius
+    within_radius = distance_values[:, 1:] < (radius + 1e-6)
+    receivers_imgs = index_array[within_radius]
+    num_neighbors_per_position = within_radius.sum(-1)
+
+    # We construct our senders and receiver indexes.
+    senders = np.repeat(np.arange(num_positions), list(num_neighbors_per_position))  # type: ignore
+    receivers_img_torch = torch.tensor(receivers_imgs, device=positions.device)
+    # Map back to indexes on the central image.
+    receivers = receivers_img_torch % num_positions
+    senders_torch = torch.tensor(senders, device=positions.device)
+
+    # Finally compute the vector displacements between senders and receivers.
+    vectors = supercell_positions[receivers_img_torch] - positions[senders_torch]
+    return torch.stack((senders_torch, receivers), dim=0), vectors
 
 # strat:
     # 1. use knn and get all the edges without PBC
@@ -67,3 +109,6 @@ def fast(*, lattice, frac_coord, radius: int = 5, max_number_neighbors: int, knn
 
 
     # how do I know the radius for the face?
+
+
+    # we don't even need tod do the transofrmation. we can just slide the lattice plane and remove coords that fall outside the plane (after we get the suepercells)
--- test.py
+++ test.py
@@ -1,27 +1,39 @@
+import numpy as np
 from create_graph import compute_pbc_radius_graph
+from fast_approach import fast
 from prep_datasets import load_dataset
+import torch
 
 
 if __name__ == "__main__":
-    knn_library = "scipy"
+    knn_library = "pynanoflann"
     dataset = load_dataset("datasets/alexandria_hdf5/train_10.h5")
     print(dataset)
     for config in dataset:
-        frac_coord = config.frac_coord
-        lattice = config.lattice
-        atomic_numbers = config.atomic_numbers
+        frac_coord = torch.tensor(config.frac_coord, dtype=torch.float32)
+        lattice = torch.tensor(config.lattice, dtype=torch.float32)
+        atomic_numbers = torch.tensor(config.atomic_numbers, dtype=torch.int64)
         print(f"frac_coord: {frac_coord}")
         print(f"lattice: {lattice}")
         print(f"atomic_numbers: {atomic_numbers}")
 
-        edges, displacements = compute_pbc_radius_graph(
-            frac_coord,
-            lattice,
-            atomic_numbers,
+        edges1, displacements1 = compute_pbc_radius_graph(
+            positions=frac_coord,
+            periodic_boundaries=lattice,
             radius=1.0,
             max_number_neighbors=20,
+            brute_force=False,
             library=knn_library
-            # brute_force=True
         )
 
-        # assert the edge index is the same between the two approaches
+        edges2, displacements2 = fast(
+            lattice=lattice,
+            frac_coord=frac_coord,
+            radius=1.0,
+            max_number_neighbors=20,
+            knn_library=knn_library
+        )
+
+        # assert edges and displacements are the same
+        assert np.allclose(edges1, edges2)
+        assert np.allclose(displacements1, displacements2)
