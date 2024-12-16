ed92a6877bf23c3f1aac8754bb1205033da75965
Curtis Chong <curtischong5@gmail.com>
2024-12-16 14:55:48 -0800

    fast2 will only prune the supercells (#1)
    * fast2 will only prune the supercells
    
    * backup before switching to cart coords leq comparison
    
    * fast3 works
    
    * update tests for fast

--- fast2.py
+++ fast2.py
@@ -1,96 +1,63 @@
-import torch
-
 from create_graph import NUM_OFFSETS, OFFSETS, _compute_img_positions_torch, points_in_parallelepiped
-from fast_approach import extend_lattice
+import torch
+from fast_approach import masked_positions_to_graph
 
 
-def get_sub_parallelepiped_for_supercell_side(lattice: torch.Tensor, supercell_side: torch.Tensor, radius: int):
+def extend_lattice2(lattice, radius):
     lengths = torch.linalg.norm(lattice, axis=1)
     unit_vectors = lattice / lengths[:, None]
-    extended_lattice = lattice + radius * unit_vectors
-    extended_lattice = extended_lattice - (radius * unit_vectors//2)
-    return extended_lattice
 
-def _compute_img_positions_torch2(
-    positions: torch.Tensor, periodic_boundaries: torch.Tensor
-) -> torch.Tensor:
-    """Computes the positions of the periodic images of the input structure.
+    additional_lengths = (radius * unit_vectors)
+    extended_lattice = lattice + additional_lengths
+    position_offset = torch.sum(-additional_lengths/2, dim=0) # dim=0 cause we want to sum up all the contributions along the x-axis (for example)
+    return extended_lattice, position_offset
 
-    Consider the following 2D periodic boundary image.
-    + --- + --- + --- +
-    |     |     |     |
-    + --- + --- + --- +
-    |     |  x  |     |
-    + --- + --- + --- +
-    |     |     |     |
-    + --- + --- + --- +
+# OFFSETS
 
-    Each tile in this has an associated translation to translate
-    'x'. For example, the top left would by (-1, +1). These are
-    the 'OFFSETS', but OFFSETS are for a 3x3x3 grid.
+def create_mask(num_atoms: int, radius: torch.Tensor, lattice: torch.Tensor):
+    leq_mask = torch.zeros(27, 3)
+    coord_mul = torch.zeros(27, 3)
+    for i in range(NUM_OFFSETS):
+        offset = OFFSETS[i]
 
-    This is complicated by the fact that our periodic
-    boundaries are not orthogonal to each other, and so we form a new
-    translation by taking a linear combination of the unit cell axes.
+        translated = lattice + offset
 
-    Args:
-        positions (torch.Tensor): Positions of the atoms. Shape [num_atoms, 3].
-        periodic_boundaries (torch.Tensor): Periodic boundaries of the unit cell.
-            This can be 2 shapes - [3, 3] or [num_atoms, 3, 3]. If the shape is
-            [num_atoms, 3, 3], it is assumed that the PBC has been repeat_interleaved
-            for each atom, i.e this function is agnostic as to whether it is computing
-            with respect to a batch or not.
-    Returns:
-        torch.Tensor: The positions of the periodic images. Shape [num_atoms, 27, 3].
-    """
-    num_positions = len(positions)
+        for j in range(3):
+            offset_i = offset[j]
+            if offset_i < 0:
+                coord_mul[i][j] = -1
+                leq_mask[i][j] = -(1 - radius_in_frac_amount)
+            elif offset_i > 0:
+                coord_mul[i][j] = 1
+                leq_mask[i][j] = radius_in_frac_amount
+            
 
-    has_unbatched_pbc = periodic_boundaries.shape == (3, 3)
-    if has_unbatched_pbc:
-        periodic_boundaries = periodic_boundaries.unsqueeze(0)
-        periodic_boundaries = periodic_boundaries.expand(num_positions, 3, 3)
+    leq_mask = leq_mask.expand(num_atoms, 27, 3)
+    coord_mul = coord_mul.expand(num_atoms, 27, 3)
+    return coord_mul, leq_mask
 
-    # This section *assumes* we have already repeat_interleaved the periodic
-    # boundaries to be the same size as the positions. e.g:
-    # (batch_size, 3, 3) -> (batch_n_node, 3, 3)
-    assert periodic_boundaries.shape[0] == positions.shape[0]
-    # First, create a tensor of offsets where the first axis
-    # is the number of particles
-    # Shape (27, 3)
-    offsets = torch.tensor(OFFSETS, device=positions.device, dtype=positions.dtype)
-    # Shape (1, 27, 3)
-    offsets = torch.unsqueeze(offsets, 0)
-    # Shape (batch_n_node, 27, 3)
-    repeated_offsets = offsets.expand(num_positions, NUM_OFFSETS, 3)
-    # offsets is now size (batch_n_node, 27, 3). Now we want a translation which is
-    # a linear combination of the pbcs which is currently shape (batch_n_node, 3, 3).
-    # Make offsets shape (batch_n_node, 27, 3, 1)
-    repeated_offsets = torch.unsqueeze(repeated_offsets, 3)
-    # Make pbcs shape (batch_n_node, 1, 3, 3)
-    periodic_boundaries = torch.unsqueeze(periodic_boundaries, 1)
-    # Take the linear combination.
-    # Shape (batch_n_node, 27, 3, 3)
-    translations = repeated_offsets * periodic_boundaries
-    # Shape (batch_n_node, 27, 3)
-    translations = translations.sum(2)
+def fast2(*, lattice: torch.Tensor, frac_coord: torch.Tensor, radius: int = 5, max_number_neighbors: int, knn_library: str, n_workers: int = 1):
 
-    # Expand the positions so we can broadcast add the translations per PBC image.
-    # Shape (batch_n_node, 1, 3)
-    expanded_positions = positions.unsqueeze(1)
-    # Broadcasted addition. Shape (batch_n_node, 27, 3)
-    translated_positions = expanded_positions + translations
-    return translated_positions
+    frac_coord = frac_coord
+    cart_coord = frac_coord @ lattice
 
-def fast(*, lattice: torch.Tensor, frac_coord: torch.Tensor, radius: int = 5, max_number_neighbors: int, knn_library: str, n_workers: int = 1):
-    cart_coords = frac_coord @ lattice
+    cart_supercell_coords = _compute_img_positions_torch(frac_coord, lattice)
 
-    # radius_for_side
-    supercell_positions = _compute_img_positions_torch2(
-        positions=cart_coords, periodic_boundaries=lattice
-    )
 
-    lattice2 = extend_lattice(lattice, radius)
+    coord_mul, leq_mask = create_mask(frac_coord.shape[0], radius, lattice)
+    mask = torch.less_equal(coord_mul * cart_supercell_coords, leq_mask)
+    # cart_supercell_coords = cart_supercell_coords.reshape(-1, 3)
+
+    extended_lattice, position_offset = extend_lattice(lattice, radius)
+
+    lattice2_mask = points_in_parallelepiped(extended_lattice, position_offset, cart_supercell_coords)
+    supercell2_positions = torch.masked_select(cart_supercell_coords, lattice2_mask.unsqueeze(-1)) # these are the relevant positions of the supercell (the ones closest to the lattice)
+
+    num_positions = len(cart_coord)
+    node_id = torch.arange(num_positions).unsqueeze(-1).expand(num_positions, NUM_OFFSETS)
 
     # the simplest way: just make a larger parallelepiped, then cross product each point with the larger parallelpied, and keep the points with a smaller cross product
-    lattice2_mask = points_in_parallelepiped(lattice2, supercell_positions)
-    supercell2_positions = torch.masked_select(supercell_positions, lattice2_mask.unsqueeze(-1))
+    node_id2 = torch.masked_select(node_id.reshape(-1), lattice2_mask)
+
+
+    return masked_positions_to_graph(supercell2_positions.reshape(-1, 3), positions=cart_coord, node_id2=node_id2, radius=radius, max_number_neighbors=max_number_neighbors, n_workers=n_workers)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from create_graph import compute_pbc_radius_graph
+from fast3 import fast3
 from fast_approach import fast
 from prep_datasets import load_dataset
 import torch
@@ -8,8 +9,8 @@ import torch
 if __name__ == "__main__":
     knn_library = "pynanoflann"
     dataset = load_dataset("datasets/alexandria_hdf5/train_10.h5")
-    print(dataset)
-    for config in dataset[1:2]:
+    # print(dataset)
+    for i, config in enumerate(dataset):
         frac_coord = torch.tensor(config.frac_coord, dtype=torch.float32)
         lattice = torch.tensor(config.lattice, dtype=torch.float32)
         atomic_numbers = torch.tensor(config.atomic_numbers, dtype=torch.int64)
@@ -17,7 +18,7 @@ if __name__ == "__main__":
         print(f"lattice: {lattice}")
         print(f"atomic_numbers: {atomic_numbers}")
 
-        radius=5.0
+        radius=1.0
 
         edges1, displacements1 = compute_pbc_radius_graph(
             positions=frac_coord,
@@ -28,7 +29,7 @@ if __name__ == "__main__":
             library=knn_library
         )
 
-        edges2, displacements2 = fast(
+        edges2, displacements2 = fast3(
             lattice=lattice,
             frac_coord=frac_coord,
             radius=radius,
@@ -37,5 +38,8 @@ if __name__ == "__main__":
         )
 
         # assert edges and displacements are the same
+        assert edges1.shape == edges2.shape
         assert np.allclose(edges1, edges2)
         assert np.allclose(displacements1, displacements2)
+        print(f"success: {i}")
+    print("we can caculate the graph properly!")
