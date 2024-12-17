bf0dac335d3b1919d22eb02f24bacebb554088c6
Curtis Chong <curtischong5@gmail.com>
2024-12-17 13:48:03 -0800

    Better err metric to compare the difference between two knn approaches (#3)

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from prep_datasets import load_dataset
 import torch
 
 def edges_to_tuples(edges, displacements):
-    assert edges.shape[1] == displacements.shape[0]
+    assert edges.shape[1] == displacements.shape[0], f"{edges.shape}, {displacements.shape}"
     tuples = []
     edgest = edges.transpose(0, 1)
     for i in range(edges.shape[1]):
@@ -19,35 +19,47 @@ def edges_to_tuples(edges, displacements):
         # tuples.append((displacement[0].item(), displacement[1].item(), displacement[2].item()))
     return sorted(tuples)
 
-def graphs_are_equal(edges1, edges2, displacements1, displacements2):
-    assert edges1.shape == edges2.shape
+
+# here's how we would make a better error metric to compare how similar two diff knn methods are:
+# 1. sort by displacements. make sure that we have nearly the same number of neighbors that are near. it's okay to have different amounts of neighbors that are far (closer to the cutoff point)
+#    - This is because in NNPs, there is a radial cutoff function. so the further the neighbor, the less their contribution is during message passing
+#    - this means that the number of neighbors don't matter
+# 2. for edge errors that are not the EXACT same, check to see if it's the same displacement. If it is, it's okay. it shouldn't contribute much to the err between the two knn methods
+def graphs_are_equal(edges1, edges2, displacements1, displacements2, ith_graph):
+    assert edges1.shape == edges2.shape, f"edges1: {edges1.shape}, edges2: {edges2.shape}"
     assert displacements1.shape == displacements2.shape
-    tuples1 = edges_to_tuples(edges1, displacements1)
-    tuples2 = edges_to_tuples(edges2, displacements2)
-    print("tuples1")
-    for t in tuples1:
-        print(t)
-    print("tuples2")
-    for t in tuples2:
-        print(t)
-    for tuple1, tuple2 in zip(tuples1, tuples2):
-        for i in range(len(tuple1)):
-            assert tuple1[i] == tuple2[i], f"{tuple1} != {tuple2}"
+    tuples1 = set(edges_to_tuples(edges1, displacements1))
+    tuples2 = set(edges_to_tuples(edges2, displacements2))
+
+    symmetric_difference = tuples1.symmetric_difference(tuples2)
+    num_mismatch = len(symmetric_difference)
+    set_size = len(tuples1)
+    err_rate = num_mismatch / (2*set_size)
+    if err_rate > 0.2:
+        print("tuples1")
+        for t in tuples1:
+            print(t)
+        print("tuples2")
+        for t in tuples2:
+            print(t)
+        print(f"ith_graph: ", ith_graph)
+        print(f"num_mismatch: {num_mismatch}")
+        print(f"set_size: {set_size}")
+        print(f"err_rate: {err_rate}")
+        exit(1)
+    return err_rate
 
 
 if __name__ == "__main__":
     torch.set_printoptions(sci_mode=False)  # Disable scientific notation
     knn_library = "pynanoflann"
-    dataset = load_dataset("datasets/alexandria_hdf5/train_10.h5")
+    dataset = load_dataset("datasets/alexandria_hdf5/train_all.h5")
     for i, config in enumerate(dataset):
         frac_coord = torch.tensor(config.frac_coord, dtype=torch.float32)
         lattice = torch.tensor(config.lattice, dtype=torch.float32)
 
         cart_coord = frac_coord @ lattice
         atomic_numbers = torch.tensor(config.atomic_numbers, dtype=torch.int64)
-        print(f"frac_coord: {frac_coord}")
-        print(f"lattice: {lattice}")
-        print(f"atomic_numbers: {atomic_numbers}")
 
         radius=5
 
@@ -68,6 +80,6 @@ if __name__ == "__main__":
             knn_library=knn_library
         )
 
-        graphs_are_equal(edges1, edges2, displacements1, displacements2)
-        print(f"success: {i}")
+        err = graphs_are_equal(edges1, edges2, displacements1, displacements2, i)
+        print(f"success: {i}. err={err}")
     print("we can calculate the graph properly!")
